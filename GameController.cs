0a11b4af2c0c63af0c549178d611fb2f2b59f626
Celia Wagar <evilagram@yahoo.com>
2026-05-08 18:32:57 -0700

    Update GameController.cs
    Added newer code

--- GameController.cs
+++ GameController.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    const int BUFFERLEN = 180;
     public static int dpad = 5;
     public static int hori = 0;
     public static int vert = 0;
+    bool faceLeft = false;
+    InputAction directionAction;
+
+    // Charge Timers
+    int chargeTimerBack = 0;
+    int chargeTimerDown = 0;
+    int chargeTimerForward = 0;
+    int chargeTimerUp = 0;
+    const int MINCHARGETIME = 60;
+    const int CHARGEUSETIME = 12;
+    const int CHARGETOOSLOW = -12;
+    bool sfstrictcharge = false;
+    int useTimerBack = 0;
+    int useTimerDown = 0;
+    int useTimerForward = 0;
+    int useTimerUp = 0;
     
     // example inputs being created for some common motions
     inputMotion shoryu = new inputMotion("Shoryuken!").Add(6,8,true).Add(2,8,false).Add(6,12,false); //fluent code, baybee!
@@ -22,133 +38,230 @@ public class GameController : MonoBehaviour
     inputMotion qcfx2 = new inputMotion("Super!").Add(2,12,true).Add(6,12,true).Add(2,12,true).Add(6,12,true);
     inputMotion hcbf = new inputMotion("Half Circle Back Forward!").Add(6,12,true).Add(2,24,false).Add(4,12,false).Add(6,12,true);
 
-
+    inputMotion[] motionQueue;
+    inputMotion[] failedMotionQueue;
 
     public static List<int> dirBuffer = new List<int>();
     private void Awake() {
+        motionQueue = new inputMotion[]{tightspin720, tightrspin720, spin720, rspin720, strictSPD, strictRSPD, SPD, RSPD, hcbf, hcfb, qcfx2, qcbx2, revElectric, revShoryu, electric, shoryu, hcf, hadouken, hcb, tatsu, downdown};
+        failedMotionQueue = new inputMotion[]{slowShoryu, slowRevShoryu, slowhcf, slowhcb, slowHadouken, slowTatsu, fastShoryu, fastRevShoryu, fasthcb, fasthcf, fastHadouken, fastTatsu, badHadou, badTatsu};
         //sets application to vsync at 60fps
         QualitySettings.vSyncCount = 1;
-
         //ignored because vsync is on, but if vsync were turned off, would fall back to this
         Application.targetFrameRate = 60;
+        
+        playerInput.actions["Direction"].performed += OnDir;
+        directionAction = InputSystem.actions.FindAction("Direction");
     }
     void Start()
     {
+        directionAction = InputSystem.actions.FindAction("Direction");
         dirBuffer = new List<int>();
     }
+
+    public void OnDir(InputAction.CallbackContext context)
+    {
+        int hori = Sign(directionAction.ReadValue<Vector2>()[0]);
+        int vert = Sign(directionAction.ReadValue<Vector2>()[1]);
+        int dpad = hori + 2 + ((vert + 1) * 3);
+        if(dpad == 5) return;
+    }
+    public static int Sign(float value)
+    {
+        if (value < 0) return -1;
+        else if (value > 0) return 1;
+        else if (value == 0) return 0;
+        return 0;
+    }
+    
+    // Update is called once per frame
     void Update()
     {
-        //all of this makes a single directional value out of a bunch of possible inputs, eventually this would be rerouted to rebindable keys
-        if(!Input.GetKey("left") && !Input.GetKey("right") && !Input.GetKey("up") && !Input.GetKey("down")){
-            dpad = 5;
-            hori = 0;
-            vert = 0;
-        }
-        else{
-            if(Input.GetKey("left") && Input.GetKey("right") || !Input.GetKey("left") && !Input.GetKey("right")){
-                hori = 0;
-            }
-            else if(Input.GetKey("left")){
-                hori = -1;
-            }
-            else if(Input.GetKey("right")){
-                hori = 1;
-            }
-
-            if(Input.GetKey("up") && Input.GetKey("down") || !Input.GetKey("up") && !Input.GetKey("down")){
-                vert = 0;
-            }
-            else if(Input.GetKey("down")){
-                vert = -1;
-            }
-            else if(Input.GetKey("up")){
-                vert = 1;
-            }
-            dpad = hori + 2 + ((vert + 1) * 3); //hashes the directions together to make a single number without a ton of if/elses
-        }
-        while(dirBuffer.Count > 60){ //clears inputs past the buffer length, which I've set to 60, should technically be a variable
+        hori = Sign(directionAction.ReadValue<Vector2>()[0]);
+        vert = Sign(directionAction.ReadValue<Vector2>()[1]);
+        dpad = hori + 2 + ((vert + 1) * 3);
+        ball.transform.position = splinePath.Spline[dpad].Position + splinePath.EvaluatePosition(0,0.0f);
+    }
+    void FixedUpdate()
+    {
+        while (dirBuffer.Count > BUFFERLEN)
+        { //clears inputs past the buffer length
             dirBuffer.RemoveAt(dirBuffer.Count - 1);
         }
-        if(Input.GetKeyDown("left") || Input.GetKeyDown("right") || Input.GetKeyDown("up") || Input.GetKeyDown("down") ||
-        Input.GetKeyUp("left") || Input.GetKeyUp("right") || Input.GetKeyUp("up") || Input.GetKeyUp("down")){ //checks if there is any change to directional keys this frame
-            //dirBuffer.Add(dpad);
-            dirBuffer.Insert(0,dpad);
-        }else{
-            //dirBuffer.Add(5); //5 is ignored
-            dirBuffer.Insert(0,5);
-        }
+        dirBuffer.Insert(0, dpad);
 
-        //this is the correct way inputs are supposed to be checked
-        if(Input.GetKeyDown("space")){
-            if(shoryu.checkValidInput()){
-                Debug.Log("success returning");
+        //Charge timer code
+        switch(dpad){
+            case 4:
+            if(faceLeft){
+                chargeTimerForward ++;
+                chargeTimerBack = 0;
+            } else{
+                chargeTimerBack ++;
+                chargeTimerForward = 0;
             }
-        }
-
-
-        //older test code
-        //lots of hardcoded values here
-        if(Input.GetKeyDown("l")){
-            for(int i = dirBuffer.Count - 1; i > dirBuffer.Count - 12; i--){
-                Debug.Log(dirBuffer[i]);
-                if(dirBuffer[i] == 5) continue;
-                if(checkDir(dirBuffer[i],6,false)){
-                    Debug.Log("first check success, " + dirBuffer[i]);
-                    for(int j = i  - 1; j > i - 12; j--){
-                        if(dirBuffer[j] == 5) continue;
-                        if(checkDir(dirBuffer[j],2,false)){
-                            Debug.Log("Second check success, " + dirBuffer[j]);
-                            for(int k = j  - 1; k > j - 12; k--){
-                                if(dirBuffer[k] == 5) continue;
-                                if(checkDir(dirBuffer[k],6,false)){
-                                    Debug.Log("Shoryuken! " + dirBuffer[k]);
-                                    break;
-                                }
-                            }
-                            break;
-                        }
-                    }
-                    break;
-                }
+            chargeTimerDown = 0;
+            chargeTimerUp = 0;
+            break;
+            case 2:
+            chargeTimerDown ++;
+            chargeTimerBack = 0;
+            chargeTimerForward = 0;
+            chargeTimerUp = 0;
+            break;
+            case 6:
+            if(faceLeft){
+                chargeTimerBack ++;
+                chargeTimerForward = 0;
+            } else{
+                chargeTimerForward ++;
+                chargeTimerBack = 0;
             }
-        }
-
-        if(Input.GetKeyDown("j")){
-            if(checkDir(3,6,false)){
-                Debug.Log("first check success");
-                if(checkDir(1,2,false)){
-                    Debug.Log("Second check success");
-                    if(checkDir(6,6,true)){
-                        Debug.Log("Shoryuken!");
-                    }
-                }
+            chargeTimerDown = 0;
+            chargeTimerUp = 0;
+            break;
+            case 8:
+            chargeTimerUp ++;
+            chargeTimerBack = 0;
+            chargeTimerDown = 0;
+            chargeTimerForward = 0;
+            break;
+            case 1:
+            if(faceLeft){
+                chargeTimerForward ++;
+                chargeTimerBack = 0;
+            } else{
+                chargeTimerBack ++;
+                chargeTimerForward = 0;
             }
+            chargeTimerDown ++;
+            chargeTimerUp = 0;
+            break;
+            case 3:
+            if(faceLeft){
+                chargeTimerBack ++;
+                chargeTimerForward = 0;
+            } else{
+                chargeTimerForward ++;
+                chargeTimerBack = 0;
+            }
+            chargeTimerDown ++;
+            chargeTimerUp = 0;
+            break;
+            case 9:
+            if(faceLeft){
+                chargeTimerBack ++;
+                chargeTimerForward = 0;
+            } else{
+                chargeTimerForward ++;
+                chargeTimerBack = 0;
+            }
+            chargeTimerUp ++;
+            chargeTimerDown = 0;
+            break;
+            case 7:
+            if(faceLeft){
+                chargeTimerForward ++;
+                chargeTimerBack = 0;
+            } else{
+                chargeTimerBack ++;
+                chargeTimerForward = 0;
+            }
+            chargeTimerUp ++;
+            chargeTimerDown = 0;
+            break;
+            case 5:
+            case 0:
+            chargeTimerBack = 0;
+            chargeTimerDown = 0;
+            chargeTimerForward = 0;
+            chargeTimerUp = 0;
+            break;
         }
+        if(chargeTimerBack >= MINCHARGETIME){
+            useTimerBack = 12;
+        } else if (useTimerBack > CHARGETOOSLOW) useTimerBack --;
+        if(chargeTimerDown >= MINCHARGETIME){
+            useTimerDown = 12;
+        } else if (useTimerDown > CHARGETOOSLOW) useTimerDown --;
+        if(chargeTimerForward >= MINCHARGETIME){
+            useTimerForward = 12;
+        } else if (chargeTimerForward > CHARGETOOSLOW) useTimerForward --;
+        if(chargeTimerUp >= MINCHARGETIME){
+            useTimerUp = 12;
+        } else if (useTimerUp > CHARGETOOSLOW) useTimerUp --;
     }
-    //this is test code, no longer being used
-    public bool checkDir(int curDir, int targetDir, bool strict){
-        //step 1, correct the currently held direction based on the facing direction, so 6 is forward, and 4 is back.
-        if (false /*facing left*/){ //TODO: add a way to detect facing direction
-            if (curDir == 7 || curDir == 4 || curDir == 1) curDir += 2;
-            else if (curDir == 9 || curDir == 6 || curDir == 3) curDir -= 2;
+
+    bool checkCharge(string button){
+        //If we want to switch this to street fighter strictness, we can replace Sign(directionAction.ReadValue<Vector2>()[0]) with dpad == 6
+        if(sfstrictcharge){
+            if(dpad == 6) hori = 1;
+            else if(dpad == 4) hori = -1;
+            else if (dpad == 8) vert = 1;
+            else if (dpad == 4) vert = -1;
+            else {
+                hori = 0;
+                vert = 0;
+            }
+        }else{
+            hori = Sign(directionAction.ReadValue<Vector2>()[0]);
+            vert = Sign(directionAction.ReadValue<Vector2>()[1]);
         }
-        
-        if (strict == true){
-            if (curDir == targetDir) return true;
-            else return false;
+        if(faceLeft){
+            if(hori < 0 && useTimerBack > 0){
+                successMessage.text = button + " Sonniku Bewm!";
+                Debug.Log(button + " Sonniku Bewm!");
+                return true;
+            } else if (hori < 0 && useTimerBack > CHARGETOOSLOW){
+                successMessage.text = button + " expired charge back-forward!";
+                Debug.Log(button + " expired charge back-forward!");
+                return false;
+            }
+            if(hori > 0 && useTimerForward > 0){
+                successMessage.text = button + " Mico Ruseo!";
+                Debug.Log(button + " Mico Ruseo!");
+                return true;
+            } else if (hori > 0 && useTimerForward > CHARGETOOSLOW){
+                successMessage.text = button + " expired charge forward-back!";
+                Debug.Log(button + " expired charge forward-back!");
+                return false;
+            }
         }
-        else { //do some fuzzy comparison shit
-            //else ifs let it terminate if it finds a value before the others
-            //the diagonals are at the bottom because it's less likely to have an input with fuzzy diagonals, so you do less comparisons
-            //the ands let it early terminate if the target direction isn't right.
-            if (targetDir == 6 && (curDir == 6 || curDir == 9 || curDir == 3)) return true;
-            else if (targetDir == 4 && (curDir == 4 || curDir == 1 || curDir == 7)) return true;
-            else if (targetDir == 2 && (curDir == 2 || curDir == 1 || curDir == 3)) return true;
-            else if (targetDir == 8 && (curDir == 8 || curDir == 7 || curDir == 9)) return true;
-            else if (targetDir == 3 && (curDir == 3 || curDir == 6 || curDir == 2)) return true;
-            else if (targetDir == 1 && (curDir == 1 || curDir == 4 || curDir == 2)) return true;
-            else if (targetDir == 9 && (curDir == 9 || curDir == 8 || curDir == 6)) return true;
-            else if (targetDir == 7 && (curDir == 7 || curDir == 8 || curDir == 4)) return true;
+        if(hori > 0 && useTimerBack > 0){
+            successMessage.text = button + " Sonniku Bewm!";
+            Debug.Log(button + " Sonniku Bewm!");
+            return true;
+        } else if (hori > 0 && useTimerBack > CHARGETOOSLOW){
+            successMessage.text = button + " expired charge back-forward!";
+            Debug.Log(button + " expired charge back-forward!");
+            return false;
+        }
+        if(hori < 0 && useTimerForward > 0){
+            successMessage.text = button + " Mico Ruseo!";
+            Debug.Log(button + " Mico Ruseo!");
+            return true;
+        } else if (hori < 0 && useTimerForward > CHARGETOOSLOW){
+            successMessage.text = button + " expired charge forward-back!";
+            Debug.Log(button + " expired charge forward-back!");
+            return false;
+        }
+        if(vert > 0 && useTimerDown > 0){
+            successMessage.text = button + " Somasalt!";
+            Debug.Log(button + " Somasalt!");
+            return true;
+        } else if (vert > 0 && useTimerDown > CHARGETOOSLOW){
+            successMessage.text = button + " expired charge down-up!";
+            Debug.Log(button + " expired charge down-up!");
+            return false;
+        }
+        if(vert < 0 && useTimerUp > 0){
+            successMessage.text = button + " Beedrill!";
+            Debug.Log(button + " Beedrill!");
+            return true;
+        } else if (vert < 0 && useTimerUp > CHARGETOOSLOW){
+            successMessage.text = button + " expired charge up-down!";
+            Debug.Log(button + " expired charge up-down!");
+            return false;
         }
         return false;
     }
